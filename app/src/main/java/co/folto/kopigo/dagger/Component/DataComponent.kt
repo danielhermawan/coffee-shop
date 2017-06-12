c20c23a6d1cc550d978fba00ecc621cca0b8de24
Daniel Hermawan <danielhermawan96@gmail.com>
2017-06-12 18:54:19 +0700

    Fix dagger depedency

--- app/src/main/java/co/folto/kopigo/dagger/Component/DataComponent.kt
+++ app/src/main/java/co/folto/kopigo/dagger/Component/DataComponent.kt
@@ -3,7 +3,6 @@ package co.folto.kopigo.dagger.Component
 import co.folto.kopigo.dagger.module.ApplicationModule
 import co.folto.kopigo.dagger.module.LocalDataModule
 import co.folto.kopigo.dagger.module.RemoteDataModule
-import co.folto.kopigo.data.OrderRepository
 import dagger.Component
 import javax.inject.Singleton
 
@@ -13,5 +12,5 @@ import javax.inject.Singleton
 @Singleton
 @Component(modules = arrayOf(ApplicationModule::class, LocalDataModule::class, RemoteDataModule::class))
 interface DataComponent {
-    fun orderRepository(): OrderRepository
+
 }
--- app/src/main/java/co/folto/kopigo/data/OrderRepository.kt
+++ app/src/main/java/co/folto/kopigo/data/OrderRepository.kt
@@ -1,9 +1,11 @@
 package co.folto.kopigo.data
 
 import co.folto.kopigo.data.contract.OrderContract
+import javax.inject.Singleton
 
 /**
  * Created by Daniel on 6/12/2017 for Kopigo project.
  */
+@Singleton
 class OrderRepository: OrderContract {
 }
